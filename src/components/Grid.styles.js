ab58194091c3019f44c45a68b5720b4eebf05385
German Arcila <germanarcila@gmail.com>
2020-09-19 14:37:07 -0400

    change colours on background and borders

--- src/components/Grid.styles.js
+++ src/components/Grid.styles.js
@@ -7,12 +7,14 @@ export default createUseStyles({
     gridTemplateColumns: 'repeat(8, 1fr)',
     width: '50vmin',
     height: '50vmin',
-    border: '2px solid black',
+    border: '4px solid #1a1a1a',
+    borderRadius: '10px',
+    backgroundColor: '#0a0a0a',
   },
   cell: {
     cursor: 'pointer',
     borderRadius: '50%',
-    border: '2px solid black',
+    border: '2px solid #222',
     background: 'white',
     transition: 'all 200ms linear',
     '&:hover': {
